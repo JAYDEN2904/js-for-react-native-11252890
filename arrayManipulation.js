b8da4dd2478ffffc4077c2be27408e80a802564a
JAYDEN2904 <osafojayden290405@gmail.com>
2024-05-17 23:44:48 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -2,4 +2,11 @@ function processArray (arr) {
   return arr.map(num => (num % 2 === 0) ? num ** 2 : num ** 3);
 }
 
+function formatArrayStrings(strArr, numArr) {
+  return strArr.map((str, index) =>
+   (numArr[index] % 2 === 0) ? str.toUpperCase( ) : str.toLowerCase( ) );
+}
 
+module.exports = {
+  processArray, formatArrayStrings
+};
